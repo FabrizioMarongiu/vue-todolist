34d58672982156ef6395e161969325bfc6db9b6c
FabrizioMarongiu <78483709+FabrizioMarongiu@users.noreply.github.com>
2021-04-22 15:25:55 +0200

    v

---
+++ js/main.js
@@ -0,0 +1,21 @@
+const app = new Vue({
+    
+    el: '#app',
+    data: {
+        ricette:[
+            {
+                text: 'Torta della Nonna',
+                tested: false,
+            },
+            {
+                text: 'Tiramisù',
+                tested: false,
+            },
+            {
+                text: 'Crema pasticcera',
+                tested: false,
+            },
+        ],
+        
+    },
+});
